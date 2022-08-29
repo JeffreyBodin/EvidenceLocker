d23042564b9e590ae84536567601a5d577045326
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2022-08-29 16:25:17 -0400

    Bump version: 1.0.25 → 1.0.26

--- evidencelocker/assets/js/site.js
+++ evidencelocker/assets/js/site.js
@@ -101,11 +101,11 @@ $("#dark-mode-toggle").click(function(){
   post('/toggle_darkmode',
     callback=function(){
       var s = $('#mainstyle')
-      if( s.prop('href').endsWith('light.css?v=1.0.25')){
-        s.prop('href','/assets/style/dark.css?v=1.0.25')
+      if( s.prop('href').endsWith('light.css?v=1.0.26')){
+        s.prop('href','/assets/style/dark.css?v=1.0.26')
       }
       else{
-        s.prop('href','/assets/style/light.css?v=1.0.25')
+        s.prop('href','/assets/style/light.css?v=1.0.26')
       }
     })
 })
