e29d5fd9ab6b4f07c13ad68d22a67cc510da0f3f
The Codeforge <92867553+TheCodeForge@users.noreply.github.com>
2022-08-30 11:45:21 -0400

    Update exhibits.py

--- evidencelocker/routes/exhibits.py
+++ evidencelocker/routes/exhibits.py
@@ -305,16 +305,16 @@ def get_locker_username_exhibit_eid_anything_signature(username, eid, anything):
         return jsonify({"error":"Exhibit is not signed"}), 400
 
     data={
-        "json_for_sig": mistletoe.markdown(pformat(e.json_for_sig)),
-        "live_sha256_with_fresh_image_hash": e.live_sha256_with_fresh_image_hash,
-        "signing_sha256": e.signing_sha256,
-        "signed_string": e.signed_string,
-        "sig_valid": e.sig_valid_with_fresh_image
+        "json_for_sig": mistletoe.markdown(pformat(exhibit.json_for_sig)),
+        "live_sha256_with_fresh_image_hash": exhibit.live_sha256_with_fresh_image_hash,
+        "signing_sha256": exhibit.signing_sha256,
+        "signed_string": exhibit.signed_string,
+        "sig_valid": exhibit.sig_valid_with_fresh_image
         }
 
-    if e.image_sha256:
+    if exhibit.image_sha256:
         data.update({
-            "pic_permalink": e.pic_permalink
+            "pic_permalink": exhibit.pic_permalink
             })
 
     return jsonify(data)
